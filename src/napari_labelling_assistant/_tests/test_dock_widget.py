22e591aa0f2fc2e882d6862ee0d16ba7c283ba82
Pranjal Dhole <pranjal.dhole@dzne.de>
2022-01-20 14:56:50 +0100

    code cov test stats function

--- src/napari_labelling_assistant/_tests/test_dock_widget.py
+++ src/napari_labelling_assistant/_tests/test_dock_widget.py
@@ -17,3 +17,35 @@ def test_example_q_widget(make_napari_viewer, capsys):
     # read captured output and check that it's as we expected
     captured = capsys.readouterr()
     assert captured.out == "napari has 1 layers\n"
+
+def test_stats_feature(make_napari_viewer, capsys):
+    viewer = make_napari_viewer()
+
+    label_array = np.array([[1, 0, 0, 1, 1, 1, 0, 1, 0, 1],
+                            [1, 0, 2, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 2, 1, 2, 1, 1, 0, 0, 2],
+                            [2, 0, 0, 1, 2, 2, 1, 1, 1, 1],
+                            [1, 2, 1, 1, 0, 0, 0, 0, 0, 1],
+                            [0, 0, 0, 0, 1, 0, 1, 2, 0, 2],
+                            [0, 0, 2, 1, 2, 1, 0, 2, 1, 0],
+                            [1, 0, 0, 1, 2, 1, 0, 0, 0, 2],
+                            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+                            [1, 0, 0, 0, 0, 0, 0, 1, 2, 1]])
+    viewer.add_labels(label_array)
+
+    my_widget = LabellingAssistant(viewer)
+
+    my_widget._std_stats()
+
+    captured = capsys.readouterr()
+
+    text = """--------------- Generating stats ------------------
+    Number of labelling layers being analysed: 1
+
+    Aggregated statistics over 1 layers:
+    Label ID: 0 | Count (in Pixels): 52 | unlabelled pixels
+    Label ID: 1 | Count (in Pixels): 32 | background pixels
+    Label ID: 2 | Count (in Pixels): 16
+    --------------- done ------------------"""
+
+    assert captured.out == text
